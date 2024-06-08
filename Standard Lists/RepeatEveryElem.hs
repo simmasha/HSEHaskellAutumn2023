00699c6ec9678af7c23e0249911d0f6bea961ad4
simmasha <120249387+simmasha@users.noreply.github.com>
2024-06-08 21:05:01 +0300

    Create RepeatEveryElem.hs

---
+++ Standard Lists/RepeatEveryElem.hs
@@ -0,0 +1,11 @@
+{- 
+Задание : 
+Повторите каждый элемент списка заданное число раз.
+
+repeatEveryElem :: Int -> [a] -> [a]
+repeatEveryElem = undefined
+GHCi> repeatEveryElem 3 "abcd"
+"aaabbbcccddd"
+-}
+repeatEveryElem :: Int -> [a] -> [a]
+repeatEveryElem n = concatMap (replicate n)
