2f48fbbb4edee66c93235d6cad3ecdb5b9143d48
simmasha <120249387+simmasha@users.noreply.github.com>
2024-06-08 21:00:05 +0300

    Create ContainsAllDigits.hs

---
+++ Standard Lists/ContainsAllDigits.hs
@@ -0,0 +1,18 @@
+{- 
+Задание : 
+Определите, содержит ли заданное целое число все цифры от 1 до 9. (Используйте функцию digits из предыдущего задания.)
+
+containsAllDigits :: Integer -> Bool
+containsAllDigits = undefined
+GHCi> containsAllDigits (-123455556789)
+True
+-}
+digits :: Integer -> [Integer]
+digits n
+    | n<0 = digits (-n)
+    | n<10 = [n]
+    | otherwise = digits (n `div` 10) ++ [n `mod` 10]
+
+containsAllDigits :: Integer -> Bool
+containsAllDigits n = all (`elem` digitList) [1..9]
+    where digitList = digits n
