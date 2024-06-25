9baf1bc69101b8a602e35c1fdcd8c160d9e0cb6a
simmasha <120249387+simmasha@users.noreply.github.com>
2024-06-25 15:36:33 +0300

    Create 4. LoggTToMonadState.hs

---
+++ Monad Transformers/4. LoggTToMonadState.hs
@@ -0,0 +1,72 @@
+{- 
+Задание 4: 
+Избавьтесь от необходимости ручного подъема операций вложенной монады State, сделав трансформер LoggT, примененный к монаде с интерфейсом MonadState, представителем этого (MonadState) класса типов:
+
+instance MonadState s m => MonadState s (LoggT m) where
+  get   = undefined
+  put   = undefined
+  state = undefined
+Тест:
+
+logSt' :: LoggT (State Integer) Integer      
+logSt' = do 
+  modify (+1)                   -- no lift!
+  a <- get                      -- no lift!
+  write2log $ show $ a * 10
+  put 42                        -- no lift!
+  return $ a * 100
+Ожидаемый результат:
+
+GHCi> runState (runLoggT logSt') 2
+(Logged "30" 300,42)
+-}
+
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+import Control.Monad
+import Control.Monad.Identity
+import Control.Monad.Cont
+import Control.Monad.State
+data Logged a = Logged String a deriving (Eq,Show)
+
+newtype LoggT m a = LoggT { runLoggT :: m (Logged a) }
+
+instance Monad m => Functor (LoggT m) where
+  fmap = liftM
+
+instance Monad m => Applicative (LoggT m) where
+  pure = return
+  (<*>) = ap
+
+instance Monad m => Monad (LoggT m) where
+  return  = LoggT . return . Logged mempty
+  m >>= k  = LoggT $ do
+      Logged v a <- runLoggT m
+      Logged u b <- runLoggT (k a)
+      return (Logged (u <> v) b)
+instance MonadFail m => MonadFail (LoggT m) where
+  fail = LoggT . fail
+
+write2log :: Monad m => String -> LoggT m ()
+write2log s = LoggT $ return $ Logged s ()
+
+type Logg = LoggT Identity
+
+logSt :: LoggT (State Integer) Integer
+logSt = do
+  lift $ modify (+1)
+  a <- lift get
+  write2log $ show $ a * 10
+  lift $ put 42
+  return $ a * 100
+runLogg :: Logg a -> Logged a
+runLogg = runIdentity . runLoggT
+
+instance MonadTrans LoggT where
+  lift = LoggT . fmap (Logged mempty)
+
+instance MonadState s m => MonadState s (LoggT m) where
+    get  =  LoggT (gets (Logged mempty))
+    put   = (LoggT . fmap (Logged mempty)) . put
+    state = (LoggT . fmap (Logged mempty)) . state
